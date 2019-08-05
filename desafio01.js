a22e64cc40a772c6f27003af9491d852753e7673
Logon Aluno <logonaluno@local.com>
2019-08-05 20:29:22 -0300

    First nodejs experience

---
+++ desafio01.js
@@ -0,0 +1,9 @@
+// Separar de 1 a 100, os números pares dos impares
+// Rodando com NodeJS
+for (i=0; i < 101; i++){
+    if(i%2==0){
+        console.log(i + " é par.");
+    }else{
+        console.log(i + " é impar");
+    }
+}
---
+++ primeiro.js
@@ -0,0 +1,8 @@
+// console.log('Brincando com n NodeJS');
+
+
+for (let n = 0; n < 11; n++) {
+    //const element = array[index];
+    console.log(n);
+    
+}
