4b6542d533abf9867128a97521f38a4a9f98268c
Logon Aluno <logonaluno@local.com>
2019-08-05 20:49:42 -0300

    XPTO

--- pagina.js
+++ pagina.js
@@ -2,8 +2,16 @@ var http = require('http'); // incorporando a biblioteca/modulo do http
 
 //Criar um servidor
 var server = http.createServer(function(req, resp){
-        resp.end("<html><body>Página de Teste</body></html>");
+        var categoria = req.url;
 
+        if(categoria == "/cerveja"){
+            resp.end("<html><body>Tudo sobre cervejas</body></html>");
+        }else if(categoria == "/tecnologia"){
+            resp.end("<html><body>Tudo sobre tecnologia</body></html>");
+        }else{
+            resp.end("<html><body>Outra página</body></html>");
+        }
+        
     });
 
 // Server estára escutando a porta 3000
