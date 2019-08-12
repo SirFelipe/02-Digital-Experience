f3dac7783a8f315a8d1fbc9e5d111e3440ba608a
Sir Felipe <felipe.dioliveira@outlook.com>
2019-08-12 20:08:41 -0300

    Desafio 02 - Media FIAP

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 // IMPORTANDO O EXPRESS
 var express = require('express');
+// npm install express
 
 // INICIALIZANDO O EXPRESS
 var app = express();
@@ -13,6 +14,23 @@ app.set('view engine', 'ejs');
 app.get('/', function(req, resp){
     resp.send("<html><body>Oi Oi Gente</body></html>");
 });
+
+app.get('/media', function(req,resp){
+    var mensagem = '<center><h2>Desafio Média</h2>';
+    //MÉDIA (NAC, AM, PS)
+    var percNac = 0.2;
+    var percAm = 0.3;
+    var percPs = 0.5;
+    var nac = req.query.nac;
+    var am = req.query.am;
+    var ps = req.query.ps;
+    var media = (nac*percNac) + (am*percAm) + (ps*percPs);
+    var arredonda = (Math.round(media*100)/100)
+    mensagem += 'Sua média é: ' + media;
+    mensagem += '</center>'
+    resp.send(mensagem);
+})
+
 
 app.get('/cerveja', function(req, resp){
     resp.render('secao/cerveja');
