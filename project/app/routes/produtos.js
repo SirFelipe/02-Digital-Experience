97fa623411cdbf6d57d0de8f24dd730e2e662e3e
Logon Aluno <felipe.dioliveira@outlook.com>
2019-08-19 20:50:41 -0300

    connecting to mysql

--- project/app/routes/produtos.js
+++ project/app/routes/produtos.js
@@ -1,5 +1,23 @@
 module.exports = function(app){
     app.get('/produtos', function(req, resp){
-        resp.render('produtos/lista');
+        var mysql = require('mysql');
+        
+        var connection = mysql.createConnection({
+                port: '3306',
+                host: 'localhost',
+                user: 'root',
+                password: '',
+                database: 'sistema_produto'
+            }
+        );
+
+        connection.query('SELECT * FROM tb_produto', function(error, result){
+            console.log(result);
+            console.log(error);
+            resp.send(result);
+        });
+
+
+        //resp.render('produtos/lista');
     });
 }
