5569a28cf802fff1a19c7fa56d72a72cd63d3a21
Logon Aluno <felipe.dioliveira@outlook.com>
2019-08-19 21:49:13 -0300

    Rendering the mysql data to the html

--- project/app/routes/produtos.js
+++ project/app/routes/produtos.js
@@ -13,11 +13,10 @@ module.exports = function(app){
 
         connection.query('SELECT * FROM tb_produto', function(error, result){
             console.log(result);
-            console.log(error);
-            resp.send(result);
+            //console.log(error);
+            //resp.send(result);
+            resp.render('produtos/produtos', {produto : result});
         });
 
-
-        //resp.render('produtos/lista');
     });
 }
