eb175c37e0d1a18802e5cb1864edb14ea275a528
Logon Aluno <felipe.dioliveira@outlook.com>
2019-09-23 20:57:32 -0300

    Validation in the client insertion

--- projeto_nodejs/app/routes/cadastrar-cliente.js
+++ projeto_nodejs/app/routes/cadastrar-cliente.js
@@ -1,13 +1,31 @@
 module.exports = function(app){
 
 app.get('/cadastrar-cliente', function(req, res){
-  res.render('admin/form_add_cliente');
+  res.render('admin/form_add_cliente', {validacao: {}, cliente: {}});
 });
 
 app.post('/clientes/salvar', function(req, res){
   var cliente = req.body;
   //res.send(cliente);
 
+  //console.log(cliente) //DEBUG
+
+  //Validação NotEmpty
+  req.assert('nome', 'O campo nome é obrigatório').notEmpty();
+  req.assert('sobrenome', 'O campo sobrenome é obrigatório').notEmpty();
+  req.assert('cep', 'O campo CEP é obrigatório').notEmpty();
+  //Validação 
+  req.assert('endereco', 'O campo endereço é obrigatório').len(10,200)
+  req.assert('cpf', 'O campo CPF é obrigatório').len(11)
+  req.assert('rg', 'O campo RG é obrigatório').len(9)
+
+  var erros = req.validationErrors();
+  console.log(erros);
+  if(erros){
+    //console.log('caiu no if');
+    res.render('admin/form_add_cliente', {validacao : erros, cliente: cliente} );
+    return;
+  }
   //CONEXAO
   //MODEL (DAO)
   //FUNCAO
